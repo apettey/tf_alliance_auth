861b3269013268dd12d45b7549de2aec3f17296b
Andrew Pettey <andrew@pettey.me>
2024-08-18 17:53:51 +0200

    chore: fix redis

--- main.tf
+++ main.tf
@@ -67,52 +67,7 @@ resource "aws_ecs_task_definition" "allianceauth_web" {
       image            = var.AA_DOCKER_IMAGE
       essential        = false
       workingDirectory = "/home/allianceauth"
-      environment = [
-        {
-          name  = "DOMAIN"
-          value = var.DOMAIN
-        },
-        {
-          name  = "AUTH_SUBDOMAIN"
-          value = var.AUTH_SUBDOMAIN
-        },
-        {
-          name  = "AA_DOCKER_IMAGE"
-          value = var.AA_DOCKER_IMAGE
-        },
-        {
-          name  = "AA_REDIS"
-          value = var.AA_REDIS
-        },
-        {
-          name  = "AA_SITENAME"
-          value = var.AA_SITENAME
-        },
-        {
-          name  = "AA_SECRET_KEY"
-          value = var.AA_SECRET_KEY
-        },
-        {
-          name  = "AA_DB_HOST"
-          value = var.AA_DB_HOST
-        },
-        {
-          name  = "AA_DB_NAME"
-          value = var.AA_DB_NAME
-        },
-        {
-          name  = "AA_DB_USER"
-          value = var.AA_DB_USER
-        },
-        {
-          name  = "AA_DB_PASSWORD"
-          value = var.AA_DB_PASSWORD
-        },
-        {
-          name  = "AA_EMAIL_HOST"
-          value = var.AA_EMAIL_HOST
-        }
-      ]
+      environment = var.CONTAINER_ENVIRONMENT
       command = [
         "python",
         "/home/allianceauth/myauth/manage.py",
@@ -126,7 +81,7 @@ resource "aws_ecs_task_definition" "allianceauth_web" {
           "awslogs-stream-prefix" = "ecs"
         }
       }
-    },{
+      }, {
       name             = "allianceauth_check"
       image            = var.AA_DOCKER_IMAGE
       essential        = false
@@ -195,52 +150,7 @@ resource "aws_ecs_task_definition" "allianceauth_web" {
       name             = "allianceauth"
       image            = var.AA_DOCKER_IMAGE
       workingDirectory = "/home/allianceauth"
-      environment = [
-        {
-          name  = "DOMAIN"
-          value = var.DOMAIN
-        },
-        {
-          name  = "AUTH_SUBDOMAIN"
-          value = var.AUTH_SUBDOMAIN
-        },
-        {
-          name  = "AA_DOCKER_IMAGE"
-          value = var.AA_DOCKER_IMAGE
-        },
-        {
-          name  = "AA_REDIS"
-          value = var.AA_REDIS
-        },
-        {
-          name  = "AA_SITENAME"
-          value = var.AA_SITENAME
-        },
-        {
-          name  = "AA_SECRET_KEY"
-          value = var.AA_SECRET_KEY
-        },
-        {
-          name  = "AA_DB_HOST"
-          value = var.AA_DB_HOST
-        },
-        {
-          name  = "AA_DB_NAME"
-          value = var.AA_DB_NAME
-        },
-        {
-          name  = "AA_DB_USER"
-          value = var.AA_DB_USER
-        },
-        {
-          name  = "AA_DB_PASSWORD"
-          value = var.AA_DB_PASSWORD
-        },
-        {
-          name  = "AA_EMAIL_HOST"
-          value = var.AA_EMAIL_HOST
-        }
-      ]
+      environment = var.CONTAINER_ENVIRONMENT
 
       portMappings = [
         {
@@ -291,52 +201,7 @@ resource "aws_ecs_task_definition" "allianceauth_workers" {
       image            = var.AA_DOCKER_IMAGE
       workingDirectory = "/home/allianceauth/myauth"
       essential        = true
-      environment = [
-        {
-          name  = "DOMAIN"
-          value = var.DOMAIN
-        },
-        {
-          name  = "AUTH_SUBDOMAIN"
-          value = var.AUTH_SUBDOMAIN
-        },
-        {
-          name  = "AA_DOCKER_IMAGE"
-          value = var.AA_DOCKER_IMAGE
-        },
-        {
-          name  = "AA_REDIS"
-          value = var.AA_REDIS
-        },
-        {
-          name  = "AA_SITENAME"
-          value = var.AA_SITENAME
-        },
-        {
-          name  = "AA_SECRET_KEY"
-          value = var.AA_SECRET_KEY
-        },
-        {
-          name  = "AA_DB_HOST"
-          value = var.AA_DB_HOST
-        },
-        {
-          name  = "AA_DB_PASSWORD"
-          value = var.AA_DB_PASSWORD
-        },
-        {
-          name  = "AA_DB_NAME"
-          value = var.AA_DB_NAME
-        },
-        {
-          name  = "AA_DB_USER"
-          value = var.AA_DB_USER
-        },
-        {
-          name  = "AA_EMAIL_HOST"
-          value = var.AA_EMAIL_HOST
-        }
-      ]
+      environment = var.CONTAINER_ENVIRONMENT
       entryPoint : [
         "sh", "-c"
       ]
@@ -357,52 +222,7 @@ resource "aws_ecs_task_definition" "allianceauth_workers" {
       image            = var.AA_DOCKER_IMAGE
       workingDirectory = "/home/allianceauth/myauth"
       essential        = true
-      environment = [
-        {
-          name  = "DOMAIN"
-          value = var.DOMAIN
-        },
-        {
-          name  = "AUTH_SUBDOMAIN"
-          value = var.AUTH_SUBDOMAIN
-        },
-        {
-          name  = "AA_DOCKER_IMAGE"
-          value = var.AA_DOCKER_IMAGE
-        },
-        {
-          name  = "AA_REDIS"
-          value = var.AA_REDIS
-        },
-        {
-          name  = "AA_SITENAME"
-          value = var.AA_SITENAME
-        },
-        {
-          name  = "AA_SECRET_KEY"
-          value = var.AA_SECRET_KEY
-        },
-        {
-          name  = "AA_DB_HOST"
-          value = var.AA_DB_HOST
-        },
-        {
-          name  = "AA_DB_PASSWORD"
-          value = var.AA_DB_PASSWORD
-        },
-        {
-          name  = "AA_DB_NAME"
-          value = var.AA_DB_NAME
-        },
-        {
-          name  = "AA_DB_USER"
-          value = var.AA_DB_USER
-        },
-        {
-          name  = "AA_EMAIL_HOST"
-          value = var.AA_EMAIL_HOST
-        }
-      ]
+      environment = var.CONTAINER_ENVIRONMENT
       entryPoint : [
         "sh", "-c"
       ]
