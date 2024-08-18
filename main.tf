e0540e617d50c07b0b45e2babd4a71645bd03406
Andrew Pettey <andrew@pettey.me>
2024-08-18 17:44:17 +0200

    chore: updated command

--- main.tf
+++ main.tf
@@ -114,7 +114,73 @@ resource "aws_ecs_task_definition" "allianceauth_web" {
         }
       ]
       command = [
-        "python /home/allianceauth/myauth/manage.py migrate",
+        "python",
+        "/home/allianceauth/myauth/manage.py",
+        "migrate",
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = var.CLOUDWATCH_LOG_GROUP
+          "awslogs-region"        = var.AWS_REGION
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    },{
+      name             = "allianceauth_check"
+      image            = var.AA_DOCKER_IMAGE
+      essential        = false
+      workingDirectory = "/home/allianceauth"
+      environment = [
+        {
+          name  = "DOMAIN"
+          value = var.DOMAIN
+        },
+        {
+          name  = "AUTH_SUBDOMAIN"
+          value = var.AUTH_SUBDOMAIN
+        },
+        {
+          name  = "AA_DOCKER_IMAGE"
+          value = var.AA_DOCKER_IMAGE
+        },
+        {
+          name  = "AA_REDIS"
+          value = var.AA_REDIS
+        },
+        {
+          name  = "AA_SITENAME"
+          value = var.AA_SITENAME
+        },
+        {
+          name  = "AA_SECRET_KEY"
+          value = var.AA_SECRET_KEY
+        },
+        {
+          name  = "AA_DB_HOST"
+          value = var.AA_DB_HOST
+        },
+        {
+          name  = "AA_DB_NAME"
+          value = var.AA_DB_NAME
+        },
+        {
+          name  = "AA_DB_USER"
+          value = var.AA_DB_USER
+        },
+        {
+          name  = "AA_DB_PASSWORD"
+          value = var.AA_DB_PASSWORD
+        },
+        {
+          name  = "AA_EMAIL_HOST"
+          value = var.AA_EMAIL_HOST
+        }
+      ]
+      command = [
+        "python",
+        "/home/allianceauth/myauth/manage.py",
+        "check",
       ]
       logConfiguration = {
         logDriver = "awslogs"
